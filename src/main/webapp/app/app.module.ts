33fb06dcf57833657a0b9f7ab08bfc164a78fed5
balvinder singh <balwinder.singh@simbalabs.com>
2018-04-14 16:55:13 +0530

    integrated MD Bootstrap Theme

--- src/main/webapp/app/app.module.ts
+++ src/main/webapp/app/app.module.ts
@@ -1,6 +1,6 @@
 import './vendor.ts';
 
-import { NgModule } from '@angular/core';
+import { NgModule, Injector, NO_ERRORS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { Ng2Webstorage } from 'ngx-webstorage';
 
@@ -12,6 +12,7 @@ import { MaterialThemeCheckAccountModule } from './account/account.module';
 import { MaterialThemeCheckEntityModule } from './entities/entity.module';
 import { customHttpProvider } from './blocks/interceptor/http.provider';
 import { PaginationConfig } from './blocks/config/uib-pagination.config';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 // jhipster-needle-angular-add-module-import JHipster will add new module here
 
@@ -35,8 +36,10 @@ import {
         MaterialThemeCheckAdminModule,
         MaterialThemeCheckAccountModule,
         MaterialThemeCheckEntityModule,
+        MDBBootstrapModule.forRoot(),
         // jhipster-needle-angular-add-module JHipster will add new module here
     ],
+    schemas: [ NO_ERRORS_SCHEMA ],
     declarations: [
         JhiMainComponent,
         NavbarComponent,
